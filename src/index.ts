b3302ea55ee5444ab1b624d9fdd74d3e08ca947e
drumil32 <akheniad@gmail.com>
2024-09-24 18:00:08 +0530

    log in db

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import winston from "winston";
 import connectDB from './config/connectDataBase.js';
 import routes from './routes/index.js';
 import moment from "moment-timezone";
+import "winston-mongodb";
 
 dotenv.config();
 
@@ -34,14 +35,31 @@ export const logger = winston.createLogger({
     // Log to the console and a file
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({ filename: "logs/app.log" }),
+        // new winston.transports.File({ filename: "logs/app.log" }),
+        new winston.transports.MongoDB({
+            level: 'info', // You can choose the log level to store in MongoDB (e.g., 'error', 'warn', 'info')
+            db: process.env.MONGO_URL, // MongoDB connection URI
+            dbName: 'logsDB',
+            collection: 'ss_logs', // The name of the collection where logs will be stored
+            options: { useNewUrlParser: true, useUnifiedTopology: true },
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json() // Store logs as JSON in MongoDB
+            ),
+        }),
     ],
 });
 
 app.use((req: Request, res: Response, next: NextFunction) => {
     // Log an info message for each incoming request
-    logger.info(`Received a ${req.method} request for ${req.url}`);
-    logger.info(JSON.stringify(req.body));
+    const requestBody = Object.keys(req.body).length ? JSON.stringify(req.body) : 'No body data';
+    
+    logger.info(`Incoming Request:
+    Method: ${req.method}
+    URL: ${req.url}
+    IP Address: ${req.ip}
+    Body: ${requestBody}`);
+    
     next();
 });
 
@@ -52,7 +70,9 @@ app.get('/health', (req: Request, res: Response) => {
 app.use('/api', routes);
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-    logger.error(err);
+    logger.error(
+        `Error occurred during ${req.method} request to ${req.url} | Status: ${err.statusCode || 500} | Message: ${err.message || "No error message"} | Stack: ${err.stack || "No stack trace"}`
+    );
     // if statusCode is there it means that message will also be created by me
     // if statusCode is not there it means that message is not created by me its something else in this situation we want to send internal server error.
     res.status(err.statusCode ? err.statusCode : 500).json({ error: err.statusCode ? err.message : 'Internal Server Error.Please try again later.' });
